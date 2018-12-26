41a2eadf9b728aff344a9b11c4066dee3cff49ea
lionel <lioneltolosaarmas@gmail.com>
2018-12-26 20:03:04 +0100

    Input Component

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
+
+  @Input() group: FormGroup;
+  @Input() field: string;
+  @Input() inputType: string;
+  @Input() placeHolder: string;
+  @Input() nameField: string;
 
   constructor() { }
 
